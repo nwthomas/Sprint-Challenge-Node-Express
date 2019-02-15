0d0fefe8efafda58e8e4c6656a7528a508ff8515
Nathan Thomas <nwthomas@me.com>
2019-02-15 09:40:20 -0800

    Finish CRUD operations for actions with .delete() request API

--- server.js
+++ server.js
@@ -133,4 +133,19 @@ server.put("/api/actions/:id", async (req, res) => {
   }
 });
 
+server.delete("/api/actions/:id", async (req, res) => {
+  try {
+    const action = await ActionsDB.remove(req.params.id);
+    if (action) {
+      res.status(200).json({ message: "The action has been deleted." });
+    } else {
+      res.status(404).json({ message: "The action could not be found." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error deleting the action."
+    });
+  }
+});
+
 module.exports = server;
