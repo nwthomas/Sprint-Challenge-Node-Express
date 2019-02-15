f1fd7ac48a6ac9c2f4a311cce69332e34d8135cc
Nathan Thomas <nwthomas@me.com>
2019-02-15 09:10:37 -0800

    Add .get() and .post() request for projects CRUD operations

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
 const express = require("express");
+const server = express();
 const helmet = require("helmet");
-const cors = require("cors");
+const ProjectDB = require("./data/helpers/projectModel.js");
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
-const server = express();
+
+// server.use("/api/projects", projectsRouter);
 
 server.get("/", (req, res) => {
-  res.status(200).json("You've reached the server! Please choose a new route!");
+  res.send(`<h1>Dude!</h1>`);
+});
+
+server.get("/api/projects/:id", async (req, res) => {
+  try {
+    const project = await ProjectDB.get(req.params.id);
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).json({ message: "No project found." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error retrieving the posts."
+    });
+  }
+});
+
+server.post("/api/projects", async (req, res) => {
+  if (!req.body.name || !req.body.description) {
+    res
+      .status(400)
+      .json("Please include a name, description, and competion status");
+  }
+  try {
+    const project = await ProjectDB.insert(req.body);
+    res.status(201).json(project);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error adding the project."
+    });
+  }
 });
 
 module.exports = server;
