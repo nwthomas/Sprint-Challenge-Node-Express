694e9d449e5c9d60123885137c867260a72ae1ae
Nathan Thomas <nwthomas@me.com>
2019-02-15 08:20:06 -0800

    Build first / route and add cors

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
-
+const cors = require("cors");
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
 const server = express();
+
+server.get("/", (req, res) => {
+  res.status(200).json("You've reached the server! Please choose a new route!");
+});
 
 module.exports = server;
